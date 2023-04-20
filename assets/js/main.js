abead80f22c4f6316c45d64d60d2db35c60ff49a
Andrea Bellome <andrea.bellome@gmail.com>
2023-04-20 21:33:27 +0200

    barred text with vue

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,20 @@ createApp({
     data() {
       return {
         /* variables Vue 3 */
-        
+        todos: [
+            {
+                text: 'Fare i compiti',
+                done: false
+            },
+            {
+                text: 'Fare la spesa',
+                done: true
+            },
+            {
+                text: 'Fare il bucato',
+                done: false
+            }
+        ]
       }
     },
     methods: {
